6a7ac7ef7d3f62a5cd58a663218e0dadbb3d10e9
fredovova <filyukhinv@bk.ru>
2021-05-26 23:48:12 +0300

    add first

---
+++ Project17/Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class MyClass
+{
+public:
+	int *temp;
+	MyClass(int size)
+	{
+		this->temp = new int[size];
+		for (int i = 0; i < size; i++)
+		{
+			temp[i] = i;
+		}
+		cout << " Вызов конструктор " <<this<< endl;
+	}
+	~MyClass()
+	{
+		cout << " Вызов деструктора " <<this<< endl;
+		delete[] temp;
+		
+	}
+};
+void foo(MyClass value)
+{
+	cout << "Вызов Фуу" << endl;
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	MyClass a(4);
+	MyClass b(a);
+	//foo(a);
+	
+	return 0;
+}
